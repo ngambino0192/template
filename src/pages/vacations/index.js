46f03191fdb11424fd80160d94b8dca3378c5e34
Nick Gambino <ngambino0192@gmail.com>
2019-06-22 21:33:34 -1000

    merge development

---
+++ src/pages/vacations/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Vacations = () => {
+  return (
+    <>
+      <div>hello world</div>
+      <div>this is a new route page</div>
+    </>
+  );
+};
+
+export default Vacations;
