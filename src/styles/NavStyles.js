e2e01f8da7f2dca95cf84fecad60cdad88b25d0f
Hannah Kidwell <kidwellhe@gmail.com>
2019-06-28 22:56:12 -1000

    Updated nav bar using bootstrap styles, added nav scroll

--- src/styles/NavStyles.js
+++ src/styles/NavStyles.js
@@ -1,7 +1,1 @@
-import styled from 'styled-components';
-
-export const LinkContainer = styled.div`
-  font-size: 25px;
-  text-align: center;
-  /* color: palevioletred; */
-`;
+import styled from "styled-components";
